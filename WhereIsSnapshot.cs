7a4bfd3ed6d4947c1300159394b8d259e5b508da
adriann88 <34092615+adriann88@users.noreply.github.com>
2017-11-30 10:27:23 +0200

    Update WhereIsSnapshot.cs

--- WhereIsSnapshot.cs
+++ WhereIsSnapshot.cs
@@ -1,12 +1,22 @@
-public static IQueryable<TEntity> WhereIsSnapshot<TEntity>(IQueryable<TEntity> query, Expression<Func<ISnapshot, bool>> predicate) 
-            where TEntity : class, IDeletable
-        {            
-            query = ((IQueryable<ISnapshot>)query)
-                .Where(predicate)
-                .Cast<TEntity>();
-            return query;
-        }
-public static IQueryable<T> ActiveSet<T>(this System.Data.Entity.DbContext context) 
+///QueryableExtensions.cs
+using EntityFramework.Extensions;
+using System;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Project
+{
+    public static class QueryableExtensions
+    {
+        /// <summary>
+        /// Gets only the records that are neither deleted nor snapshots.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
+        public static IQueryable<T> ActiveSet<T>(this System.Data.Entity.DbContext context) 
             where T : class, IDeletable 
         {
             string currentType = typeof(T).FullName;
@@ -18,3 +28,86 @@ public static IQueryable<T> ActiveSet<T>(this System.Data.Entity.DbContext conte
             }
             return context.Set<T>().Where(f => f.IsDeleted == false);
         }
+
+        /// <summary>
+        /// Returns a query that filters entities based of the implementation of the ISnapshot interface
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="predicate">The filtering condition.</param>
+        /// <returns></returns>
+        public static IQueryable<TEntity> WhereIsSnapshot<TEntity>(IQueryable<TEntity> query, Expression<Func<ISnapshot, bool>> predicate) 
+            where TEntity : class, IDeletable
+        {            
+            query = ((IQueryable<ISnapshot>)query)
+                .Where(predicate)
+                .Cast<TEntity>();
+            return query;
+        }        
+    }
+}
+
+///Interfaces used in the code above
+    /// <summary>
+    /// Logical deletable entity
+    /// </summary>
+    public interface IDeletable
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is deleted.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is deleted; otherwise, <c>false</c>.
+        /// </value>
+        /// <remarks>
+        /// This will mark the entity as deleted, instead of physically removing it.
+        /// </remarks>
+        bool IsDeleted { get; set; }
+    }
+
+    /// <summary>
+    /// Logical snapshot entity
+    /// </summary>
+    public interface ISnapshot
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is a snapshot.
+        /// </summary>
+        /// <value>
+        /// If the value is true, this instance should not be present in Context
+        /// </value>        
+        bool IsSnapshot { get; set; }
+    }
+
+    /// <summary>
+    /// Gets only SnapshotEntities
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    IQueryable<T> IDataContext.SnapshotEntities<T>()
+    {
+        return Set<T>().Where(f => f.IsSnapshot == true);
+    }
+
+    /// <summary>
+    /// Gets only logically deleted entities
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    IQueryable<T> IDataContext.RecycleBin<T>()
+    {
+        return Set<T>().Where(f => f.IsDeleted == true);
+    }
+
+    /// <summary>
+    /// Gets only the active entities. Deleted and Snapshot entities are excluded.
+    /// </summary>
+    IQueryable<ComplianceMatrix> ICompanyContext.ComplianceMatrices
+    {
+        get { return this.ActiveSet<ComplianceMatrix>(); }
+    }
+
+//Example 
+
+//Gets all the snapshots of the compliance matrices 
+var cmxEntities = ctx.SnapshotEntities<ComplianceMatrix>().Where(f => complianceMatrixIds.Contains(f.Id));
